9abab6ec5f8c111cad38f3e0531ff58630c1b2d3
aishaxon <aishaxon0010@gmail.com>
2026-08-09 21:42:04 +0500

    Import xatosi tuzatildi

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,7 @@
 import secrets
 from django.contrib.auth.hashers import make_password
-from rest_framework import viewsets, status,APIView
+from rest_framework import viewsets, status
+from rest_framework.views import APIView
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
