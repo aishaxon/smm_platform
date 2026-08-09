451589a1386471b4298af3b4d6d28c366526b495
aishaxon <aishaxon0010@gmail.com>
2026-08-09 23:47:08 +0500

    Cors tog'rilandi

--- SMM_PLATFORM/settings.py
+++ SMM_PLATFORM/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
 AUTH_USER_MODEL = "users.User"
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     "corsheaders.middleware.CorsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
