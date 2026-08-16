040deb65c43003cf4ec94342c3d5405bba7d1010
aishaxon <aishaxon0010@gmail.com>
2026-08-16 19:41:36 +0500

    api-auth (session login) olib tashlandi - faqat JWT qoldi

--- SMM_PLATFORM/urls.py
+++ SMM_PLATFORM/urls.py
@@ -6,7 +6,6 @@ from apps.users.views import PhoneTokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
     path("token/", PhoneTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("users/", include("apps.users.urls")),
